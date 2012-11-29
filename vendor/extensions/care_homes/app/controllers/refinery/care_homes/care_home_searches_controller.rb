a56e3a69e23830d550a1a1bbb799009211f3db9f
Mike Jerome <jeromem@michrome.com>
2012-11-29 15:42:22 +0000

    Remove reliance on URLs

--- vendor/extensions/care_homes/app/controllers/refinery/care_homes/care_home_searches_controller.rb
+++ vendor/extensions/care_homes/app/controllers/refinery/care_homes/care_home_searches_controller.rb
@@ -4,8 +4,8 @@ module Refinery
 
       def create
         @care_homes = CareHome.near_postcode(params[:postcode])
-        # @page = ::Refinery::Page.where(:link_url => "/care_homes").first
-        # present(@page)
+        @page = ::Refinery::Page.find_by_path(refinery.care_homes_care_homes_path)
+        present(@page)
         render 'refinery/care_homes/care_homes/index'
       end
 
